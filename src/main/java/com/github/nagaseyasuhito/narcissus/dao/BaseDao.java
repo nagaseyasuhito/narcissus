485f651a922d440d0f65cb4bb9f1a2d9845ca397
nagaseyasuhito <nagase@nagaseyasuhito.net>
2013-06-09 23:39:17 +0900

    Getting EntityManager to change abstract

--- src/main/java/com/github/nagaseyasuhito/narcissus/dao/BaseDao.java
+++ src/main/java/com/github/nagaseyasuhito/narcissus/dao/BaseDao.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.SortedMap;
 
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import com.github.nagaseyasuhito.fatsia.Criterias;
@@ -13,37 +12,36 @@ import com.github.nagaseyasuhito.narcissus.entity.BaseEntity;
 
 public abstract class BaseDao<S extends Serializable, T extends BaseEntity<S>> {
 
-	@Inject
-	protected EntityManager entityManager;
+	protected abstract EntityManager getEntityManager();
 
 	public long countByCriteria(EntityCriteria<T> criteria) {
-		return Criterias.count(this.entityManager, criteria);
+		return Criterias.count(this.getEntityManager(), criteria);
 	}
 
 	public T findByCriteria(EntityCriteria<T> criteria) {
-		return Criterias.find(this.entityManager, criteria);
+		return Criterias.find(this.getEntityManager(), criteria);
 	}
 
 	public List<T> findByCriteria(EntityCriteria<T> criteria, SortedMap<String, Boolean> orders) {
-		return Criterias.find(this.entityManager, criteria, orders);
+		return Criterias.find(this.getEntityManager(), criteria, orders);
 	}
 
 	public List<T> findByCriteria(EntityCriteria<T> criteria, SortedMap<String, Boolean> orders, int firstResult, int maxResults) {
-		return Criterias.find(this.entityManager, criteria, orders, firstResult, maxResults);
+		return Criterias.find(this.getEntityManager(), criteria, orders, firstResult, maxResults);
 	}
 
 	public T findById(S id) {
-		return this.entityManager.find(this.getEntityClass(), id);
+		return this.getEntityManager().find(this.getEntityClass(), id);
 	}
 
 	public abstract Class<T> getEntityClass();
 
 	public T merge(T entity) {
-		return this.entityManager.merge(entity);
+		return this.getEntityManager().merge(entity);
 	}
 
 	public void persist(T entity) {
-		this.entityManager.persist(entity);
+		this.getEntityManager().persist(entity);
 	}
 
 	public T persistOrMerge(T entity) {
@@ -56,6 +54,6 @@ public abstract class BaseDao<S extends Serializable, T extends BaseEntity<S>> {
 	}
 
 	public void remove(T entity) {
-		this.entityManager.remove(entity);
+		this.getEntityManager().remove(entity);
 	}
 }
